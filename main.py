6bfe45c956bb06519ae337a1f4636011cce4d141
ThunderO6 <weiliao695@gmail.com>
2025-08-14 11:19:48 -0400

    Start

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class MyClient(discord.Client):
+  async def on_ready(self):
+    print('Logged on as {0}!'.format(self.user))
+
+  async def on_message(self, message):
+    if message.author == self.user:
+      return
+
+    if message.content.startswith('$hello'):
+      await message.channel.send('Hello World!')
+
+    if message.content.startswith('$start'):
+      embed = discord.Embed(
+          title="Battle Box",
+          description="Floor 1",
+          color=discord.Color.blue()
+      )
+      embed.add_field(name=message.author.name ,value= message.author.avatar.url, inline=False)
+      embed.add_field(name="Field 2", value="Another value", inline=True)
+      embed.add_field(name="Field 3", value="Another value", inline=True)
+      #embed.set_thumbnail(url=message.author.avatar.url)     
+      embed.set_footer(text="Footer text here")
+      #embed.set_author(name=message.author.name, icon_url=message.author.avatar.url)
+      await message.channel.send(embed=embed)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(os.getenv('DISCORD_TOKEN')) #Token
+
